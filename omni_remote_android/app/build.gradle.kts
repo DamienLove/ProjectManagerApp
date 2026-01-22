16aa384a3ad7b54ffa338e81a4282b74eb8adeb7
DamienLove <damienlovemusic@gmail.com>
2026-01-22 01:52:27 -0700

    build: bump Android version to 4.5.0

--- omni_remote_android/app/build.gradle.kts
+++ omni_remote_android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.damiennichols.omniremote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "4.4.0"
+        versionCode = 9
+        versionName = "4.5.0"
     }
 
     signingConfigs {
